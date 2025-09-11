8ab822b880de629880b1a699c26bdbdcad22077d
Uladzislau <u.shylko25@gmail.com>
2025-09-11 20:29:08 +0300

    feat: add useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
 import './App.css'
 import {Table} from "./Table.tsx";
+import {useState} from "react";
 
 function App() {
-  const topCars = [
-    { manufacturer: 'BMW', model: 'm5cs' },
-    { manufacturer: 'Mercedes', model: 'e63s' },
-    { manufacturer: 'Audi', model: 'rs6' },
-  ]
-  return(
-      <div>
-        <Table cars={topCars} />
-      </div>
-  )
+    const topCars = [
+        {manufacturer: 'BMW', model: 'm5cs'},
+        {manufacturer: 'Mercedes', model: 'e63s'},
+        {manufacturer: 'Audi', model: 'rs6'},
+    ]
+
+    let [a, setA] = useState(0);
+
+    const OnclickFunction = () => {
+        setA(++a);
+    }
+    const OnclickFunction1 = () => {
+        setA(a = 0);
+    }
+
+    return (
+        <div>
+            <Table cars={topCars}/>
+
+
+            <h1>{a}</h1>
+            <button onClick={OnclickFunction}>Onclick</button>
+            <button onClick={OnclickFunction1}>0</button>
+        </div>
+
+    )
 }
 
 export default App
