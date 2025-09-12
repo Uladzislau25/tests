0549f39afe3456b6d9fd60b223cedb585287170a
Uladzislau <u.shylko25@gmail.com>
2025-09-12 23:44:17 +0300

    feat: add test multiply

--- src/01/01.test.ts
+++ src/01/01.test.ts
@@ -1,4 +1,4 @@
-import {sum} from "./01.ts";
+import {multiply, sum} from "./01.ts";
 import {test, expect} from "vitest";
 
 test('sum should be correct', () => {
@@ -18,4 +18,23 @@ test('sum should be correct', () => {
 
     expect(result).toBe(3);
     expect(result1).toBe(4);
+})
+
+test('multiply should be correct', () => {
+
+
+
+    let a = 1;
+    let b = 2;
+    let c = 3;
+
+    //
+
+
+    const result = multiply(a, b);
+    const result1 = multiply(a, c);
+    // expect result
+
+    expect(result).toBe(2);
+    expect(result1).toBe(3);
 })
