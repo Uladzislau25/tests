59aa9c3899fe43692f66faef8b3eab012c26b3cb
Uladzislau <u.shylko25@gmail.com>
2025-09-12 23:40:00 +0300

    feat: add test sum

---
+++ src/01/01.test.ts
@@ -0,0 +1,21 @@
+import {sum} from "./01.ts";
+import {test, expect} from "vitest";
+
+test('sum should be correct', () => {
+
+
+
+    let a = 1;
+    let b = 2;
+    let c = 3;
+
+    //
+
+
+    const result = sum(a, b);
+    const result1 = sum(a, c);
+    // expect result
+
+    expect(result).toBe(3);
+    expect(result1).toBe(4);
+})
